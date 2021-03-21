a26c3bcd90f173c03db044583c60945fd09b31ea
Kaecio <kaecio@pop-os.localdomain>
2021-03-21 02:04:03 -0300

    Create the model and the database MongoDb

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-//const router = express.Router();
+const router = express.Router();
+
+// Conectando ao banco
+mongoose.connect("mongodb+srv://kaecio:123@cluster0.5gmzg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser:true});
+
+// carrega os models
+const Product = require("./model/product");
 
 // carrega as rotas
 const indexRoute = require("./routes/index");
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,15 +1,52 @@
 "use strict";
 
+const mongoose = require("mongoose");
+const Product = mongoose.model("Product");
+
+exports.get = (req, res, next) => {
+  Product.find({active: true},'title price slug')
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send(err);
+    });
+};
+
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({slug: req.params.slug, active: true},'title description price slug tags')
+      .then((data) => {
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        res
+          .status(400)
+          .send(err);
+      });
+  };
+
 exports.post = (req, res, next) => {
-  res.status(201).send(req.body);
+  var product = new Product(req.body);
+  // product.title = req.body.title
+  product
+    .save()
+    .then((el) => {
+      res.status(201).send({ message: "Produto codastrado com sucesso" });
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send({ message: "Falha ao cadastrar produto", data: err });
+    });
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({ id: id, items: req.body });
-  };
+  const id = req.params.id;
+  res.status(200).send({ id: id, items: req.body });
+};
 
-  exports.delete = (req, res, next) => {
-    res.status(201).send(req.body);
-  };
-  
+exports.delete = (req, res, next) => {
+  res.status(201).send(req.body);
+};
