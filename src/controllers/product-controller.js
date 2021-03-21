26404123481b2ecba531d8fc3f6ee3c44bd63b9a
Kaecio <kaecio@pop-os.localdomain>
2021-03-21 02:43:22 -0300

    Create the CRUD

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,30 +3,54 @@
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
 
+// traz tudo filtrando pelos "title price slug"
 exports.get = (req, res, next) => {
-  Product.find({active: true},'title price slug')
+  Product.find({ active: true }, "title price slug")
     .then((data) => {
       res.status(200).send(data);
     })
     .catch((err) => {
-      res
-        .status(400)
-        .send(err);
+      res.status(400).send(err);
     });
 };
 
+// traz pelo slug
 exports.getBySlug = (req, res, next) => {
-    Product.findOne({slug: req.params.slug, active: true},'title description price slug tags')
-      .then((data) => {
-        res.status(200).send(data);
-      })
-      .catch((err) => {
-        res
-          .status(400)
-          .send(err);
-      });
-  };
+  Product.findOne(
+    { slug: req.params.slug, active: true },
+    "title description price slug tags"
+  )
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
 
+// traz pelo id
+exports.getById = (req, res, next) => {
+  Product.findById(req.params.id)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+// traz pela tag
+exports.getByTag = (req, res, next) => {
+  Product.find({ tags: req.params.tag, actitive: true })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+// inserir dados
 exports.post = (req, res, next) => {
   var product = new Product(req.body);
   // product.title = req.body.title
@@ -42,11 +66,35 @@ exports.post = (req, res, next) => {
     });
 };
 
+// atualiza o produto
 exports.put = (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({ id: id, items: req.body });
+  Product.findByIdAndUpdate(req.params.id, {
+    $set: {
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      slug: req.body.slug,
+    },
+  })
+    .then((el) => {
+      res.status(201).send({ message: "Produto atualizado com sucesso" });
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send({ message: "Falha ao atualizar produto", data: err });
+    });
 };
 
+// excuindo o profuto
 exports.delete = (req, res, next) => {
-  res.status(201).send(req.body);
+  Product.findOneAndRemove(req.body.id)
+    .then((el) => {
+      res.status(201).send({ message: "Produto excluido com sucesso" });
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send({ message: "Falha ao remover produto", data: err });
+    });
 };
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const controller = require("../controllers/product-controller");
 
 router.get('/', controller.get);
+router.get('/:id', controller.getById);
 router.get('/:slug', controller.getBySlug);
+router.get('/:tag', controller.getByTag);
 router.post("/", controller.post);
 router.put("/:id", controller.put);
 router.delete("/", controller.delete);
